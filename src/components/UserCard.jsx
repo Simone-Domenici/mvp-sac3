254112e54f73e03eb8afc63259cb8f118442c8ee
Simone-Domenici <domenici.simone00@gmail.com>
2025-01-16 15:20:10 +0100

    ADD search functionality to Home page and enhance UserCard component with labeled details

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -7,9 +7,9 @@ export default function UserCard({ utente }) {
             <h4>{utente.nome} {utente.cognome}</h4>
         </div>
             <div className={moreInfo ? 'show' : 'hide'}>
-                <p>{utente.numeroTelefono}</p>
-                <p>{utente.codiceFiscale}</p>
-                <p>{utente.email}</p>
+                <p>Telefono: {utente.numeroTelefono}</p>
+                <p>CodiceFisicale: {utente.codiceFiscale}</p>
+                <p>Email: {utente.email}</p>
             </div>
     </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import { destinazioni } from "../data/data"
+import { useEffect, useState } from "react"
+
 import Card from "../components/Card"
 
 export default function Home(){
+    const [filteredDestinazioni, setFilteredDestinazioni] = useState(destinazioni)
+        
+    
+    const [search, setSearch] = useState('')
+    function filterDestination(destinazioni, search){
+        
+        return destinazioni.filter((d) => d.destinazione.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    useEffect(() => {
+        let array =[...destinazioni]
+        if (search) {
+            array = filterDestination(destinazioni, search)
+        }
+        setFilteredDestinazioni(array)
+        
+    },[search])
+
     return(
         <main>
-            {destinazioni.map((destinazione) => (
+            <div>
+                <input type="text" name='nome' value={search} placeholder='cerca...' onChange={(e) => setSearch(e.target.value)}/>            
+            </div>
+            {filteredDestinazioni.map((destinazione) => (
                 <Card key={destinazione.id} destinazione={destinazione} />
             ))}
         </main>
